7e789c8f916b5dc161a6166a67f06abf09117760
João Gabriel Lima <joao.gls2009@gmail.com>
2026-09-18 12:09:44 -0300

    feito o database

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const PORT = 5000
 
 app.get('/', (req, res)=>{
     res.send("Servidor rodando")
 })
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("servidor rodando na porta 5000")
 })
