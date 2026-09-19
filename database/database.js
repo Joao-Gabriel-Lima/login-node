8f00e2201c323f2a13919911c8ffe0d38ec8cd9b
João Gabriel Lima <joao.gls2009@gmail.com>
2026-09-19 12:51:37 -0300

    feito o servidor(serverJs) e corrigido databaseJs

--- database/database.js
+++ database/database.js
@@ -1,19 +1,11 @@
 const Database = require('better-sqlite3');
 const db = new Database('usuarios.db');
 
-const nome = "joao"
-const email = "joao@gmail.com"
-const senha = "jgls123"
-
 db.exec(`
     CREATE TABLE IF NOT EXISTS usuarios(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT,
     email TEXT UNIQUE,
     senha TEXT)`)
-
-db.prepare('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)').run(nome, email, senha)
-const resultado = db.prepare('SELECT * FROM usuarios').all()
-console.log(resultado)
 
 module.exports = db
--- server.js
+++ server.js
@@ -1,11 +1,41 @@
+//Parte de coisas baixadas
+const db = require('./database/database.js')
+const bcrypt = require('bcrypt');
 const express = require('express')
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 const PORT = 5000
+//Parte de coisas baixadas)
+
+app.listen(PORT, () => {
+    console.log("servidor rodando na porta 5000")
+})
 
 app.get('/', (req, res)=>{
     res.send("Servidor rodando")
 })
 
-app.listen(PORT, () => {
-    console.log("servidor rodando na porta 5000")
+app.post('/register',async (req, res) => {
+    const {nome, email, senha} = req.body
+
+//Parte da criptografia
+const saltRounds = 10
+const senhaHash = await bcrypt.hash(senha, saltRounds)
+//Parte da criptografia
+
+//Sql
+try{
+db.prepare('INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)').run(nome, email, senhaHash)
+const resultado = db.prepare('SELECT * FROM usuarios').all()
+console.log(resultado)
+res.status(201).json({mensagem:"Usuário criado com sucesso!"})
+} catch (erro) {
+    if(erro.code == 'SQLITE_CONSTRAINT_UNIQUE'){
+        res.status(409).json({erro:"O usuário ja existe!"})
+    }else{
+        res.status(500).json({ServerError:"Erro no servidor!"})
+    }
+}
+//Sql
 })
